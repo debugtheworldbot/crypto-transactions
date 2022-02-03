d81ae3010fbbba88e4d65dc8fd13c9ab48e3ed0a
tian <istiancz@gmail.com>
2022-02-04 00:03:56 +0800

    feat: transactions init

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,13 +1,12 @@
 import React, { useContext } from "react"
-
-// import { TransactionContext } from "../context/TransactionContext"
+import { TransactionContext } from "../context/Transaction"
 
 // import useFetch from "../hooks/useFetch"
-// import dummyData from "../utils/dummyData"
-// import { shortenAddress } from "../utils/shortenAddress"
+import dummyData from "../utils/dummyData"
+import { shortenAddress } from "../utils/shortenAddress"
 
 const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
-  const gifUrl = useFetch({ keyword })
+  // const gifUrl = useFetch({ keyword })
 
   return (
     <div className="bg-[#181918] m-4 flex flex-1
@@ -34,11 +33,11 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
             </>
           )}
         </div>
-        <img
+        {/* <img
           src={gifUrl || url}
           alt="nature"
           className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
-        />
+        /> */}
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
           <p className="text-[#37c7da] font-bold">{timestamp}</p>
         </div>
@@ -48,12 +47,12 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
 }
 
 const Transactions = () => {
-  // const { transactions, currentAccount } = useContext(TransactionContext)
+  const { transactions, currentAccount } = useContext(TransactionContext)
 
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
-        {/* {currentAccount ? (
+        {currentAccount ? (
           <h3 className="text-white text-3xl text-center my-2">
             Latest Transactions
           </h3>
@@ -61,13 +60,13 @@ const Transactions = () => {
           <h3 className="text-white text-3xl text-center my-2">
             Connect your account to see the latest transactions
           </h3>
-        )} */}
+        )}
 
-        {/* <div className="flex flex-wrap justify-center items-center mt-10">
-          {[...dummyData, ...transactions].reverse().map((transaction, i) => (
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          {[...dummyData ].reverse().map((transaction, i) => (
             <TransactionsCard key={i} {...transaction} />
           ))}
-        </div> */}
+        </div>
       </div>
     </div>
   )
